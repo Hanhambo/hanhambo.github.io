508848498bbb3e5e0be5cf900a1adcc7704eaa1c
hanhambo <honey820623@gmail.com>
2023-11-01 18:51:53 +0800

    today's update

--- index.js
+++ index.js
@@ -1,59 +1,53 @@
 // Change Block
-let diary, JS30;
+  let diary, JS30;
 
-document.addEventListener("DOMContentLoaded", function(){
-  diary = document.querySelector(".diary");
-  JS30 = document.querySelector(".JS30");
-});
+  document.addEventListener("DOMContentLoaded", function(){
+    diary = document.querySelector(".diary");
+    JS30 = document.querySelector(".JS30");
+  });
 
-function showDiary(){
-    diary.classList.add("diary");
-    diary.classList.remove("hide");
-    JS30.classList.add("hide");
-    JS30.classList.remove("JS30");
-}
-
-function showJS30(){
-    diary.classList.add("hide");
-    diary.classList.remove("diary");
-    JS30.classList.add("show");
-    JS30.classList.remove("hide");
-}
-
-// JS30-1
-function playSound(e){
-    // console.log(e.keyCode);
-
-    // 鎖定 audio 標籤讓鍵盤發出聲音
-    let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    // console.log(audio);
-
-    // 鎖定 key class 讓動態呈現
-    let key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    // console.log(key);
-
-
-    if (audio === null) { // !audio
-      return; // stop the function
-    } else {
-      audio.currentTime = 0; // rewind to start
-      audio.play();
-
-      // 增加 "playing" 這個 class 下去
-      key.classList.add("playing");
-    }
+  function showDiary(){
+      diary.classList.add("diary");
+      diary.classList.remove("hide");
+      JS30.classList.add("hide");
+      JS30.classList.remove("JS30");
   }
 
-  // 鍵盤離手之後動畫狀態需解除
-  function removeTransition(e) {
-    if (e.propertyName === 'transform') {
-        // console.log(e.propertyName);
-        e.target.classList.remove('playing');
-    }
-}
+  function showJS30(){
+      diary.classList.add("hide");
+      diary.classList.remove("diary");
+      JS30.classList.add("show");
+      JS30.classList.remove("hide");
+  }
 
-  let keys = document.querySelectorAll(".key");
-  // console.log(keys);
-  keys.forEach(key => key.addEventListener("transitionend", removeTransition));
-  window.addEventListener("keydown", playSound);
 // JS30-1
+(function(){
+
+  function playHandler(e){
+    // play music
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if (audio) { 
+      audio.currentTime = 0;
+      audio.play();
+    }
+    // transition
+    const trans = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (trans) {
+      trans.classList.add("playing");
+    }
+    // console.log(audio, trans);
+  }
+  
+  function transHandler(e) {
+    if (e.propertyName === "transform") {
+        e.currentTarget.classList.remove("playing");
+    }
+  }
+  
+  window.addEventListener("keydown", playHandler);
+  // 鍵盤離手之後動畫狀態需解除
+  document.querySelectorAll(".key").forEach(function(key){
+    key.addEventListener("transitionend", transHandler);
+  })
+
+})()
